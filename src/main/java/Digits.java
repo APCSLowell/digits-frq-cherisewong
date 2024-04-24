e090cb724c0d14bfbdad3a6171d532bfd91b2222
cherisewong <73048335+cherisewong@users.noreply.github.com>
2024-04-24 09:03:10 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -6,13 +6,27 @@ public class Digits
 	private ArrayList<Integer> digitList;
 
 	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	    
+	{
+		int sum = 0;
+		int number = num;
+		while (number > 0){
+			sum++;
+			number /= 10;
+		}
+		digitList = new ArrayList<Integer>();
+		for (int i = 0; i < sum; i++){
+			digitList.add(num % 10);
+		}
 	}
 
 	public boolean isStrictlyIncreasing()
-	{ /* to be implemented in part (b) */
-		
+	{
+		for (int i = 1; i < digitList.size(); i++){
+			if (digitList.get(i) <= digitList.get(i-1)){
+				return false;
+			}
+		}
+		return true;
 
 	}
 	
